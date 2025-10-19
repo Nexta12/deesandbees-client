8ca52f839c25e85a6b81d58d6257ec049379392a
Ernest <ernestez12@gmail.com>
2025-10-19 09:14:52 +0100

    clean up process started

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,31 +1,11 @@
-// import BriefIntroSection from "./components/briefIntro/BriefIntroSection"
-// import HeroSlider from "./components/heroSlider/HeroSlider"
-// import Services from "./components/ourServices/Services"
-// import Testimonials from "./components/testimonials/Testimonials"
 
-
-// const Homepage = () => {
-//   return (
-//     <>
-//      <HeroSlider/>
-//      <BriefIntroSection/>
-//      <Services/> 
-//      <Testimonials/>
-
-   
-//     </>
-//   )
-// }
-
-// export default Homepage
-
-import HeroSlider from "./components/heroSlider/HeroSlider";
 import BriefIntroSection from "./components/briefIntro/BriefIntroSection";
+import FAQs from "./components/faq/Faqs";
+import HeroSlider from "./components/heroSlider/HeroSlider";
 import ServicesShowcase from "./components/ourServices/ServicesShowcase";
 import WhyChooseUs from "./components/whyChooseUs/WhyChooseUs";
+import FinalCTA from "./components/finalCta/FinalCta";
 import Testimonials from "./components/testimonials/Testimonials";
-import FinalCTA from "./components/finalCTA/FinalCTA";
-import FAQs from "./components/faq/Faqs";
 
 const Homepage = () => {
   return (
